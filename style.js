8df1ce76ec22c829500a2536d73d0cfaea3c5562
Vitoria Pereira <108802117+viihpereira@users.noreply.github.com>
2023-04-14 11:51:05 -0300

    Add files via upload

---
+++ style.js
@@ -0,0 +1,8 @@
+var resposta = confirm('deseja continuar na pagína?');
+        console.log(resposta); 
+
+document.querySelector("#matricula").addEventListener("click",function () {
+        alert('No momento estamos enfrentado alguns problemas, por isso não será possível realizar a matricula.')
+});
+//var resposta = confirm('como vc esta?')
+        console.log(resposta)
